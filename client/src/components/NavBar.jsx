db80d68313e40ccf19a4f22f12b1d9fd3d99c371
Sujan <077bct087.sujan@pcampus.edu.np>
2024-06-09 13:41:05 +0545

    Nav setup

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='bg-slate-200 '>
+        <div className='flex flex-row items-center justify-between p-2'>
+            
+           <div className='m-6 text-lg'>
+             My Blogs
+            </div>
+            <div className='flex flex-row items-center justify-around'>
+            
+        
+        <div className='p-6'>Home</div>
+        <div className='p-6'>About</div>
+        <div className='p-6'>Login</div>
+        <div className='p-6'>SIgnUp</div>
+            </div>
+
+        </div>
+
+
+    </div>
   )
 }
 
