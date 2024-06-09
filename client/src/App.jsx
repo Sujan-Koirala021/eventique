2a3203b903edd8e694584390ac54b9671863a516
Sujan <077bct087.sujan@pcampus.edu.np>
2024-06-09 13:34:03 +0545

    Init setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import NavBar from './components/NavBar'
+
+
+function App() {
+
+  return (
+    <>
+    <NavBar/>    
+    <div className='text-3xl text-red-400'>Heleo  worke</div>
+    </>
+  )
+}
+
+export default App
